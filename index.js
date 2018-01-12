66147ba0d8113907fd91722ca536cf16058dfb86
T-Dex <dex.m.mills@gmail.com>
2018-01-11 23:32:44 -0700

    Built express server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3001;
+
+app.get('/', (req,res)=>
+  res.send(`NODE and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, ()=>{
+  console.log(`Your server is running on port ${PORT}`);
+});
