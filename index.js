ce92a845289b623e1367e8f10e95de7f68b4684e
T-Dex <dex.m.mills@gmail.com>
2018-01-12 10:04:45 -0700

    Routing

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes'
 
 const app = express();
 const PORT = 3001;
+
+routes(app);
 
 app.get('/', (req,res)=>
   res.send(`NODE and express server is running on port ${PORT}`)
